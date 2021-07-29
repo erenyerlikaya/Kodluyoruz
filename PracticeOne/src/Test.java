221d306541fab37c77c3273a123b3e798204c35f
Eren Yerlikaya <yerlikayaeren00@gmail.com>
2021-07-29 15:54:36 +0300

    Pratik1

---
+++ PracticeOne/src/Test.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		//Scanner sýnýfýný java.utilden import ediyoruz.
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		int mat, fizik, kimya, turkce, biyoloji;
+		
+		//kullanýcýya notlarý girdiriyoruz
+		
+		System.out.println("Matematik notunuzu giriniz : ");
+		mat = scanner.nextInt();
+		System.out.println("Fizik notunuzu giriniz : ");
+		fizik = scanner.nextInt();
+		System.out.println("Kimya notunuzu giriniz : ");
+		kimya = scanner.nextInt();
+		System.out.println("Türkçe notunuzu giriniz : ");
+		turkce = scanner.nextInt();
+		System.out.println("Biyoloji notunuzu giriniz : ");
+		biyoloji = scanner.nextInt();
+		
+		int toplam = (mat+fizik+kimya+turkce+biyoloji);
+		
+		
+		double ortalama = toplam / 5;
+		
+		System.out.println("Not ortalamanýz : " + ortalama);
+
+	}
+
+}
